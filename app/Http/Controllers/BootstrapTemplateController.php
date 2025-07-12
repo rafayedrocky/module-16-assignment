5823adafe9aa321ebef3a64d2fe4bfaf1782e656
rafayedrocky <rafayedrocky321s@gmail.com>
2025-07-13 00:24:12 +0600

    bootstrap template include in laravel blade

--- app/Http/Controllers/BootstrapTemplateController.php
+++ app/Http/Controllers/BootstrapTemplateController.php
@@ -9,6 +9,24 @@ class BootstrapTemplateController extends Controller
     public function index()
     {
         $title = "Personal - Start Bootstrap Theme";
-        return view('layout.app', compact('title'));
+        return view('home', compact('title'));
+    }
+
+    public function resume()
+    {
+        $title = "Personal - Start Bootstrap Resume";
+        return view('resume', compact('title'));
+    }
+
+    public function projects()
+    {
+        $title = "Personal - Start Bootstrap Projects";
+        return view('projects', compact('title'));
+    }
+
+    public function contact()
+    {
+        $title = "Personal - Start Bootstrap Contact";
+        return view('contact', compact('title'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('bootstrap-template-home', [BootstrapTemplateController::class, 'index']);
+Route::get('bootstrap-template/home', [BootstrapTemplateController::class, 'index'])->name('home');
+Route::get('bootstrap-template/resume', [BootstrapTemplateController::class, 'resume'])->name('resume');
+Route::get('bootstrap-template/projects', [BootstrapTemplateController::class, 'projects'])->name('projects');
+Route::get('bootstrap-template/contact', [BootstrapTemplateController::class, 'contact'])->name('contact');
